6cf4f60eb8b8638d7f14ebc930e05ae876846ff3
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-05-05 13:03:20 -0400

    added cocktail.jpeg, updated single-drink.html

---
+++ single-drink.js
@@ -0,0 +1,3 @@
+const drinkName = document.querySelector('.drink-name');
+const drinkDesc = document.querySelector('.drink-desc');
+const drinkIngredients = document.querySelector('.drink-ingredients');
