e335f6a4642c060dcf1a34732389dde574c20b1f
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-05-13 10:37:57 -0400

    updated single-drink.js

--- single-drink.js
+++ single-drink.js
@@ -9,6 +9,52 @@ function searchDrinks(searchQuery) {
   fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      const ingredientsList = getIngredients(data.drinks[0]);
+      displaySingle(data.drinks[0], ingredientsList);
     });
+}
+
+function displaySingle(data, ingredientsList) {
+  console.log(data);
+  const drinkName = document.querySelector('.drink-name');
+  const drinkDesc = document.querySelector('.drink-desc');
+  const drinkImage = document.querySelector('.drink-img');
+  const drinkIngredientsList = document.querySelector('.drink-ingredients');
+  const activeIngredients = ingredientsList.filter((value) => value != null);
+
+  drinkName.innerText = data.strDrink;
+  drinkDesc.innerText = data.strInstructions;
+  drinkImage.src = data.strDrinkThumb;
+
+  const activeIngredientsList = activeIngredients
+    .map((ingredient) => {
+      return `<li>
+        <i class = "far fa-check-square"></i>
+        ${ingredient}
+      </li>`;
+    })
+    .join('');
+
+  console.log(activeIngredientsList);
+
+  drinkIngredientsList.innerHTML = activeIngredientsList;
+
+  document.querySelector('.loading').classList.add('hide-loading');
+}
+
+function getIngredients(data) {
+  const ingredientsList = [
+    data.strIngredient1,
+    data.strIngredient2,
+    data.strIngredient3,
+    data.strIngredient4,
+    data.strIngredient5,
+    data.strIngredient6,
+    data.strIngredient7,
+    data.strIngredient8,
+    data.strIngredient9,
+    data.strIngredient10,
+  ];
+
+  return ingredientsList;
 }
